77eb1752520674469e4bcdf9e14f5aacf468b1ff
Kevin Trinh <kxt.trinh@gmail.com>
2022-12-22 16:06:40 +0100

    Reset scroll position and forms

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  // On strrt
+  connect() {
+    console.log("Connected")
+    const messages = document.getElementById("messages");
+    messages.addEventListener("DOMNodeInserted", this.resetScroll);
+    this.resetScroll(messages);
+  }
+
+  // On Stop
+  disconnect() {
+    console.log("Disconnected")
+  }
+
+  // Custom function
+  resetScroll() {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
