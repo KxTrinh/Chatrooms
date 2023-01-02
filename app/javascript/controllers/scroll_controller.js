f04bef1a23813de603c59c5e284a0137a358987b
Kevin Trinh <kxt.trinh@gmail.com>
2023-01-02 19:48:47 +0100

    infinite scroll with pagy

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // On strrt
+  initialize() {
+    this.resetScrollWithoutThreshold(messages);
+  }
+
+  // On start
   connect() {
     console.log("Scroll controller connected")
     const messages = document.getElementById("messages");
     messages.addEventListener("DOMNodeInserted", this.resetScroll);
-    this.resetScroll(messages);
+    // this.resetScroll(messages);
   }
 
   // On Stop
@@ -16,6 +20,15 @@ export default class extends Controller {
 
   // Custom function
   resetScroll() {
+    const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
+    const upperScrollThreshold = bottomOfScroll - 500;
+    // Scroll down if we're not withing the threshold
+    if (messages.scrollTop > upperScrollThreshold) {
+      messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+  }
+  
+  resetScrollWithoutThreshold(messages) {
     messages.scrollTop = messages.scrollHeight - messages.clientHeight;
   }
 }
