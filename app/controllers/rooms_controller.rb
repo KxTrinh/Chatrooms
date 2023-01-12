05c5243f83d4f5504c65454791dca5cb2f4e1b22
Kevin Trinh <kxt.trinh@gmail.com>
2023-01-12 22:18:33 +0100

    sort room by lastest message sent

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,7 +6,7 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     # @rooms = Room.public_rooms
-    @joined_rooms = current_user.joined_rooms
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
     @rooms = search_rooms
 
     @users = User.all_except(current_user)
@@ -18,7 +18,7 @@ class RoomsController < ApplicationController
     @room = Room.new
     # @rooms = Room.public_rooms
     @rooms = search_rooms
-    @joined_rooms = current_user.joined_rooms
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
 
     @message = Message.new
     # @messages = @single_room.messages.order(created_at: :asc)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding...'
+
+kev = User.create(email: 'kev@mail.com',
+                  password: '123456',
+                  password_confirmation: '123456',
+                  role: 'admin')
+User.create(email: 'john@mail.com',
+            password: '123456',
+            password_confirmation: '123456')
+User.create(email: 'jane@mail.com',
+            password: '123456',
+            password_confirmation: '123456')
+kev.joined_rooms << Room.create(name: 'General', is_private: false)
+kev.joined_rooms << Room.create(name: 'Testing', is_private: false)
+
+puts 'Finished !'
